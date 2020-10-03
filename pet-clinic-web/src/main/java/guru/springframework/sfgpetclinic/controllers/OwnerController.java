010720838e7e23bd6e94cc63e03ee03cfc7d7270
Sergiu Nagy <sergiunb@gmail.com>
2020-10-03 19:48:20 +0200

    Implement Owners index page and controller . Closes #13

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,13 +3,13 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String index(){
 
-        return "vets/index";
+        return "owners/index";
     }
 }
