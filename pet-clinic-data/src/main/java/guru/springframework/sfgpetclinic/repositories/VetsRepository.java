ab63cc3f789f8bd7adcb711cae752bf26e4cbbaf
Sergiu Nagy <sergiunb@gmail.com>
2020-10-11 18:42:25 +0200

    Add Data Repositories extending Spring JPA  repositories. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetsRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetsRepository extends CrudRepository<Vet, Long> {
+}
